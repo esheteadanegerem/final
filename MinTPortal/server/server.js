177a48b384fcaff6a29eaac692f3380d657f344e
adan eshete <adaneeshete560@gmail.com>
2024-04-04 13:28:12 +0300

    path updates

--- MinTPortal/server/server.js
+++ MinTPortal/server/server.js
@@ -76,9 +76,9 @@ app.use('/history',histroyRoute)
 app.use('/auth/:page',register)
 app.use('/announcements/:page', announcementPost);
 app.use('/authl',login)//,ProtectAdmin
-app.use('/admind')//dashboardRoute)
-app.use('/admind2')//,dashboardRoute2)
-app.use('/admind3')//,dashboardRoute3)
+// app.use('/admind')//dashboardRoute)
+// app.use('/admind2')//,dashboardRoute2)
+// app.use('/admind3')//,dashboardRoute3)
 //app.use('/announcements', fetchRoute)
 app.use('/news', news);
 //app.use('/auth',ProtectAdmin)
